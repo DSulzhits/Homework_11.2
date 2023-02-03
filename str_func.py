49d37a492449a5d8c5d653c949e95fca98005497
DSulzhits <dsulzhits@gmail.com>
2023-02-03 22:01:41 +0300

    добавил докстринг к Upper добавил функцию title

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def upper(input_word):
+    """Функция делает все все буквы заглавными"""
     return input_word.upper()
+
+def title(input_word):
+    """Функция делает заглавными только 1е буквы слова"""
+    return input_word.title()
